962aa5eef10b4ecd6e9232d347f42998bd89650c
Raul Victor Queiroz <raulvictor00@outlook.com>
2021-09-02 18:36:27 -0300

    feat: add challenges and tests

---
+++ src/phonelist/index.js
@@ -0,0 +1,17 @@
+function quickSearch(string, array) {
+  if (!Array.isArray(array)) {
+    throw new Error('Input must be an array');
+  }
+  const pivotIndex = Math.floor(array.length / 2);
+  const pivot = array[pivotIndex];
+
+  if (pivot.name > string) {
+    return quickSearch(string, array.slice(0, pivotIndex));
+  } else if (pivot.name < string) {
+    return quickSearch(string, array.slice(pivotIndex));
+  } else if (pivot.name === string) {
+    return pivot;
+  }
+}
+
+module.exports = quickSearch;
